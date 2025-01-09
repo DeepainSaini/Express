4979acf2e640888de70f4b62bf884a6fc4ec952d
DeepainSaini <deepainsaini111@gmail.com>
2025-01-09 22:12:57 +0530

    Adding routers and filters

--- app.js
+++ app.js
@@ -2,32 +2,21 @@ const express  = require('express');
 
 const app  = express();
 
+const adminRoutes = require('./routes/admin');
+
+const shopRoutes = require('./routes/shop');
+
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended : false}));
 
-app.use('/',(req,res,next)=>{
-   
-    next();
-});
+app.use('/admin',adminRoutes);
 
-app.use('/add-product',(req,res,next)=>{
+app.use('/shop',shopRoutes);
 
-    res.send('<form action="/product" method="POST"><input type"text" name="title"><br><br><input type"number" name="size"><button type="submit">Add Product</button></form>');
-    
-});
+app.use((req,res,next)=>{
 
-app.use('/product',(req,res,next)=>{
-     
-    console.log(req.body); 
-    res.redirect('/');
-});
-
-app.use('/',(req,res,next)=>{
-
-    res.send('<h1>Welcome to express js</h1>');
-});
-
-
+    res.status(404).send('<h1>Page Not found</h1>');
+})
 
 app.listen(4000);
