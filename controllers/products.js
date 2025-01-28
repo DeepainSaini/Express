4bd83b7c1ab4f0693d7c2abaf2f3767abb2ffe59
DeepainSaini <deepainsaini111@gmail.com>
2025-01-29 01:22:09 +0530

    Adding Modles

--- controllers/products.js
+++ controllers/products.js
@@ -1,3 +1,4 @@
+const Product = require('../models/products');
 
 const path = require('path');
 
@@ -14,11 +15,17 @@ exports.getAddProductPage = (req,res,next)=>{
 
 exports.postProduct = (req,res,next)=>{
      
+    const product = new Product(req.body.title);
+    product.save();
     console.log(req.body); 
     res.redirect('/shop');
 };
 
 exports.getProducts = (req,res,next)=>{
 
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    Product.fetchAll((products)=>{
+         
+        res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    });
+
 }
